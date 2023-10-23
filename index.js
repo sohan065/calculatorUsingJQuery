663b9d7651dc248daddfb2b0f24c455e2cb9da29
sohan065 <mdsohan065@gmail.com>
2023-10-24 01:23:39 +0600

    some modified

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const insertNumber = (number) => {
   var existsNumber = $("#result").val();
   var lastCharacter = existsNumber.slice(-1);
-  if (lastCharacter == "+" && lastCharacter == number) {
-    $("#result").val(existsNumber);
-  } else if (lastCharacter == "-" && lastCharacter == number) {
-    $("#result").val(existsNumber);
-  } else if (lastCharacter == "*" && lastCharacter == number) {
-    $("#result").val(existsNumber);
-  } else if (lastCharacter == "/" && lastCharacter == number) {
+
+  if (
+    (lastCharacter == "+" ||
+      lastCharacter == "-" ||
+      lastCharacter == "*" ||
+      lastCharacter == "/" ||
+      lastCharacter == ".") &&
+    lastCharacter == number
+  ) {
     $("#result").val(existsNumber);
   } else {
     $("#result").val(existsNumber + number);
+    // if (number == "-" || number == "*" || number == "/") {
+    //   var sliceNumber = $("#result").val().slice(0, -1);
+    //   alert(sliceNumber + number);
+    //   $("#result").val(sliceNumber + number);
+    // } else {
+
+    // }
   }
 };
 
@@ -19,8 +28,20 @@ const clearResult = () => {
 };
 
 const showResult = () => {
-  const result = eval($("#result").val());
-  $("#result").val(result);
+  var existsNumber = $("#result").val();
+  var lastCharacter = existsNumber.slice(-1);
+
+  if (
+    lastCharacter == "+" ||
+    lastCharacter == "-" ||
+    lastCharacter == "*" ||
+    lastCharacter == "/"
+  ) {
+    var total = eval($("#result").val().slice(0, -1));
+    $("#result").val(total);
+  }
+  var total = eval($("#result").val());
+  $("#result").val(total);
 };
 
 const deleteNumber = () => {
@@ -28,4 +49,9 @@ const deleteNumber = () => {
   if (presentValue != "") {
     $("#result").val(presentValue.slice(0, -1));
   }
+};
+
+const getPercentage = () => {
+  const totalAmount = eval($("#result").val());
+  $("#result").val(totalAmount / 100);
 };
